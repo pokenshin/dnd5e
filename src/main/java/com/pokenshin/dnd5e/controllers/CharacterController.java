a0c2bef9da13cb9a5ed1bb019d259829fcf8f36c
Jonas Augusto <pokenshin@hotmail.com>
2018-10-01 13:14:05 -0300

    Corrected Spring Boot dependency on POM and created a mock controller

---
+++ src/main/java/com/pokenshin/dnd5e/controllers/CharacterController.java
@@ -0,0 +1,8 @@
+package com.pokenshin.dnd5e.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class CharacterController {
+}
